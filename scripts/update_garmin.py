74e79acd96bc290f89804791e49642fe3aec5e98
alliebanwell-ux <alliebanwell@gmail.com>
2026-07-11 13:38:19 -0700

    Update update_garmin.py

--- scripts/update_garmin.py
+++ scripts/update_garmin.py
@@ -1,1 +1,10 @@
-print("Hello from update_garmin.py")
+import os
+
+print("Workflow is running!")
+
+password = os.environ.get("GARMIN_MAPSHARE_PASSWORD")
+
+if password:
+    print("SUCCESS: Secret found.")
+else:
+    raise Exception("Secret missing.")
